ac6be6c7ce7b4976eb26343050ad9a1c94f12008
Lucas Sell Machado <75432770+Sellucas@users.noreply.github.com>
2022-11-09 14:29:38 -0300

    searchitems

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import { BiSearch } from 'react-icons/bi'
+import { useState } from 'react'
+import { SearchItems } from './SearchItems'
 
 export const Hero = () => {
+    const [value, setValue] = useState('')
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const onSearch = (key) => {
+        setValue(key)
+        console.log('search', key)
+    }
+
     return (
         <>
             <section className='hero'>
@@ -12,13 +25,14 @@ export const Hero = () => {
                     </h1>
                     <p>High-quality Design Themes for personal or commercial use contains 6k+ items in 100 categories.</p>
                     <div className="search">
-                    <span>All Categories</span>
-                    <hr />
-                    <input type="text" placeholder='Search Products ...'/>
-                    <button>
-                        <BiSearch className='searchIcon heIcon'/>
-                    </button>
+                        <span>All Categories</span>
+                        <hr />
+                        <input type="text" placeholder='Search Products ...' onChange={onChange} value={value} />
+                        <button onClick={() => onSearch(value)}>
+                            <BiSearch className='searchIcon heIcon' />
+                        </button>
                     </div>
+                    <SearchItems product={product} value={value} onSearch={onSearch} />
                     <p>Examples: Mockip, PSD, Theme Design, Image...</p>
 
                 </div>
---
+++ src/components/home/hero/SearchItems.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { FiSearch, FiShoppingBag } from 'react-icons/fi'
+
+export const SearchItems = ({ value, product, onSearch }) => {
+    return (
+        <>
+            <section className='searchItems'>
+                <div className="product_items">
+                    {product.filter((items) => {
+                        const searchkey = value.toLowerCase()
+                        const title = items.title.toLowerCase()
+
+                        return searchkey && title.startWith(searchkey) && title != searchkey
+                    }).slice(0, 10).map((items) => (
+                        <div className="box" key={items.id}>
+                            <div className="img">
+                                <img src={items.cover} alt="" />
+                                <div className="overlay">
+                                    <button className='button'>
+                                        <FiShoppingBag />
+                                    </button>
+                                    <button className='button'>
+                                        <AiOutlineHeart />
+                                    </button>
+                                    <button className='button' onClick={() => onOpenImg(items.cover)}>
+                                        <FiSearch />
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="details">
+                                <h3>{items.title}</h3>
+                                <p>{items.author}</p>
+                                <h4>Price: ${items.price}</h4>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </section>
+        </>
+    )
+}
